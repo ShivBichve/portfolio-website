a3139ad889ca4affd6966b27372386fa2c2ea698
viral <viralpawar661@gmail.com>
2025-04-20 19:12:55 +0530

    portfolio website

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+let loader = document.getElementById("preloader");
+
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        loader.style.display = "none";
+    }, 2000);
+});
+
+
+
+$.fn.jQuerySimpleCounter = function (options) {
+    var settings = $.extend({
+        start: 0,
+        end: 100,
+        easing: 'swing',
+        duration: 400,
+        complete: ''
+    }, options);
+
+    var thisElement = $(this);
+
+    $({ count: settings.start }).animate({ count: settings.end }, {
+        duration: settings.duration,
+        easing: settings.easing,
+        step: function () {
+            var mathCount = Math.ceil(this.count);
+            thisElement.text(mathCount);
+        },
+        complete: settings.complete
+    });
+};
+
+$('#number1').jQuerySimpleCounter({ end: 100, duration: 3000 });
+$('#number2').jQuerySimpleCounter({ end: 800, duration: 3000 });
+
+/* AUTHOR LINK */
+$('.about-me-img').hover(function () {
+    $('.authorWindowWrapper').stop().fadeIn('fast').find('p').addClass('trans');
+}, function () {
+    $('.authorWindowWrapper').stop().fadeOut('fast').find('p').removeClass('trans');
+});
